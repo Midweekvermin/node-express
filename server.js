4bee9c3674861be7c512e8b84c9f77c815509762
Midweekvermin <113259156+Midweekvermin@users.noreply.github.com>
2023-05-15 16:29:03 -0600

    Add files via upload

--- server.js
+++ server.js
@@ -6,10 +6,23 @@ const app = express();
 app.use((req, res, next) => {
     console.log('hello')
     next();
-})
+});
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+app.post('/profile', (req, res) => {
+    const user = {
+        name: 'sally',
+        hobby: 'soccer',
+    }
+});
 
 app.get('/', (req,res) => {
     res.send('Happy Mothers Day Mom!!!!!')
-})
+});
+app.get('/profile',(req,res) => {
+    res.send('Getting Profile')
+});
 
 app.listen(80);

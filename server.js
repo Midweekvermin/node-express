946f2a1dce17e5548c7b871af354bf232ef48204
Midweekvermin <113259156+Midweekvermin@users.noreply.github.com>
2023-05-16 09:47:05 -0600

    Add files via upload

--- server.js
+++ server.js
@@ -3,26 +3,33 @@ const express = require('express');
 const app = express();
 
 //middleware
-app.use((req, res, next) => {
-    console.log('hello')
-    next();
-});
+// app.use((req, res, next) => {
+//     console.log('hello')
+//     next();
+// });
 
-app.use(express.urlencoded({extended: false}));
-app.use(express.json());
+// app.use(express.urlencoded({extended: false}));
+// app.use(express.json());
 
-app.post('/profile', (req, res) => {
-    const user = {
-        name: 'sally',
-        hobby: 'soccer',
-    }
-});
+// app.post('/profile', (req, res) => {
+//     const user = {
+//         name: 'sally',
+//         hobby: 'soccer',
+//     }
+// });
 
-app.get('/', (req,res) => {
-    res.send('Happy Mothers Day Mom!!!!!')
-});
-app.get('/profile',(req,res) => {
-    res.send('Getting Profile')
-});
+// app.get('/', (req,res) => {
+//     // req.query
+//     // req.body
+//     // req.header
+//     // req.params
+//     //req.status(404).send('not found');
+//     res.send('Happy Mothers Day Mom!!!!!');
+// });
+// app.get('/profile',(req,res) => {
+//     res.send('Getting Profile')
+// });
+
+app.use(express.static(__dirname + '/public'));
 
 app.listen(80);
